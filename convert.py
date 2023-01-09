8046be6e554584e4452abf544ecbae90bf1ad763
GooseGooseDk <2120651691@qq.com>
2023-01-09 18:25:15 +0800

    添加mgg2ogg方法

--- convert.py
+++ convert.py
@@ -11,7 +11,17 @@ def find_vip_folders(path=r'E:\music',vip_folder_name='VipSongsDownload'):
                 print(full_path)
     return vip_folders
 
+def mgg2ogg(input_path,output_path,core_path=r'E:\music\um.exe'):
+    cmd='%s -o %s -i "%s"' % (core_path,output_path,input_path)
+    print(cmd)
+    ord=os.popen(cmd)
+    echo=ord.read()
+    return echo
 
+# E:\music\王菲\浮躁\VipSongsDownload\王菲 - 想像.mgg
 
+input_path='E:\music\王菲\浮躁\VipSongsDownload\王菲 - 想像.mgg'
+output_path='E:\music\王菲\浮躁\VipSongsDownload'
 
-
+print(mgg2ogg(input_path,output_path))
+print('done')
